74022595e009320928e43970a32390b8ef0f8385
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-07 16:48:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fit Track
+
+Get to know the fit track feature of 24/7 Fitness.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold text-center my-4">Hello Fit Track</p>
+    </>
+  );
+}
+
+export default App;
