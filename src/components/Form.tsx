cec6777edcbcae914bfef603549f80a8164aaac7
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-09 14:00:51 +0200

    style: form area

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,13 +2,15 @@ export const Form = () => {
   return (
     <section className="bg-teal-100 py-20 px-5">
       <div className="max-w-4xl mx-auto">
-        <form className="space-y-5 bg-gray-100 p-10 border-2 rounded-lg">
+        <form className="space-y-5 bg-gray-100 shadow p-10 rounded-lg">
           <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category">Category:</label>
             <select
               className="border border-slate-300 p-2 rounded-lg w-full"
               id="category"
-            >check</select>
+            >
+              check
+            </select>
           </div>
         </form>
       </div>
