f4c8f710baab55a0f652b5e86720cf8006079b8c
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-09 14:37:51 +0200

    feat: made data and different labels with inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,55 @@
+import { categories } from '../data/categories';
+
 export const Form = () => {
   return (
     <section className="bg-teal-100 py-20 px-5">
       <div className="max-w-4xl mx-auto">
         <form className="space-y-5 bg-gray-100 shadow p-10 rounded-lg">
           <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="category">Category:</label>
+            <label htmlFor="category" className="font-bold">
+              Category:
+            </label>
             <select
               className="border border-slate-300 p-2 rounded-lg w-full"
               id="category"
             >
-              check
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
             </select>
           </div>
+
+          <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category" className="font-bold">
+              Activity:
+            </label>
+            <input
+              id="activity"
+              type="text"
+              className="border border-slate-300 p-2 rounded-lg w-full"
+              placeholder="Food or Exercise"
+            />
+          </div>
+
+          <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category" className="font-bold">
+              Calories:
+            </label>
+            <input
+              id="calories"
+              type="text"
+              className="border border-slate-300 p-2 rounded-lg w-full"
+              placeholder="Calories burned"
+            />
+          </div>
+
+          <input
+            type="submit"
+            className="bg-zinc-700 hover:bg-zinc-800 w-full p-2 text-white cursor-pointer font-bold"
+            value="Save food or exercise"
+          />
         </form>
       </div>
     </section>
---
+++ src/data/categories.ts
@@ -0,0 +1,6 @@
+import { Categories } from '../types/interface';
+
+export const categories: Categories[] = [
+  { id: 1, name: 'Food' },
+  { id: 2, name: 'Exercise' },
+];
