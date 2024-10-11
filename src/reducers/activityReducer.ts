cd35e256ba0cb257dc0f518c1dd79a1ba7fb03c0
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-11 18:02:21 +0200

    feat: By narrowing the type of the payload in your if blocks, you can safely access newActivity or id without TypeScript throwing an error.

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -2,15 +2,17 @@ import { Performance } from '../types/interface';
 
 interface ActivityState {
   activities: Performance[];
+  activeId: Performance['id'];
 }
 
 export interface ActivityActions {
-  type: 'save-activity';
-  payload: { newActivity: Performance };
+  type: 'save-activity' | 'save-activeId';
+  payload: { newActivity: Performance } | { id: Performance['id'] };
 }
 
 export const initialSate: ActivityState = {
   activities: [],
+  activeId: '',
 };
 
 export const activityReducer = (
@@ -18,9 +20,20 @@ export const activityReducer = (
   action: ActivityActions
 ) => {
   if (action.type === 'save-activity') {
+    const { newActivity } = action.payload as { newActivity: Performance };
+
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity],
+      activities: [...state.activities, newActivity],
+    };
+  }
+
+  if (action.type === 'save-activeId') {
+    const { id } = action.payload as { id: Performance['id'] };
+    
+    return {
+      ...state,
+      activeId: id,
     };
   }
 
