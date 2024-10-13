5d10b9c146b1a6c9c5fee45bf472372c9860cea3
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-13 22:05:52 +0200

    feat: In this case, checking the length of an array is not an expensive operation, so there’s no need for useMemo.

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -21,61 +21,68 @@ const ActivityList = ({ activities, dispatch }: Props) => {
   return (
     <section className="p-10 mx-auto max-w-4xl">
       <h2 className="text-4xl font-bold text-center text-slate-600">
-        Activities with Calories
+        Food and Activities with Calories
       </h2>
-      {activities.map((movement) => (
-        <div
-          key={movement.id}
-          className="px-5 py-10 mt-5 shadow-lg flex justify-between "
-        >
-          <div className="space-y-2 relative">
-            <p
-              className={`absolute -top-8 -left-8 px-10 py-2 text-white rounded-lg shadow-md uppercase font-bold ${
-                movement.category === 1 ? 'bg-teal-600' : 'bg-cyan-600'
-              }`}
-            >
-              {categoryName(movement.category)}
-            </p>
 
-            <p className="font-bold text-2xl text-slate-600 pt-5">
-              {movement.activity}
-            </p>
+      {activities.length === 0 ? (
+        <p className="text-center text-slate-600 mt-10">
+          Not Activity is done yet
+        </p>
+      ) : (
+        activities.map((movement) => (
+          <div
+            key={movement.id}
+            className="px-5 py-10 mt-5 shadow-lg flex justify-between "
+          >
+            <div className="space-y-2 relative">
+              <p
+                className={`absolute -top-8 -left-8 px-10 py-2 text-white rounded-lg shadow-md uppercase font-bold ${
+                  movement.category === 1 ? 'bg-teal-600' : 'bg-cyan-600'
+                }`}
+              >
+                {categoryName(movement.category)}
+              </p>
 
-            <p
-              className={`font-bold text-3xl ${
-                movement.category === 1 ? 'text-teal-600' : 'text-cyan-600'
-              }`}
-            >
-              {movement.calories} Calories
-            </p>
+              <p className="font-bold text-2xl text-slate-600 pt-5">
+                {movement.activity}
+              </p>
+
+              <p
+                className={`font-bold text-3xl ${
+                  movement.category === 1 ? 'text-teal-600' : 'text-cyan-600'
+                }`}
+              >
+                {movement.calories} Calories
+              </p>
+            </div>
+
+            <div className="flex gap-5 items-center">
+              <button
+                className="text-3xl text-slate-700 hover:text-slate-800"
+                onClick={() =>
+                  dispatch({
+                    type: 'set-activeId',
+                    payload: { id: movement.id },
+                  })
+                }
+              >
+                <FaRegEdit />
+              </button>
+              <button
+                className="text-3xl text-red-500 hover:text-red-600"
+                onClick={() =>
+                  dispatch({
+                    type: 'delete-activity',
+                    payload: { id: movement.id },
+                  })
+                }
+              >
+                <RiDeleteBinLine />
+              </button>
+            </div>
           </div>
-
-          <div className="flex gap-5 items-center">
-            <button
-              className="text-3xl text-slate-700 hover:text-slate-800"
-              onClick={() =>
-                dispatch({
-                  type: 'set-activeId',
-                  payload: { id: movement.id },
-                })
-              }
-            >
-              <FaRegEdit />
-            </button>
-            <button
-              className="text-3xl text-red-500 hover:text-red-600"
-              onClick={() =>
-                dispatch({
-                  type: 'delete-activity',
-                  payload: { id: movement.id },
-                })
-              }
-            >
-              <RiDeleteBinLine />
-            </button>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </section>
   );
 };
