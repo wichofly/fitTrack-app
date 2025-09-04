d9ce231ebbebe721fe9753c483e5ef6613e6fe9d
Mauricio Erazo <antonierazo13@hotmail.com>
2025-09-04 21:24:58 +0200

    Refactor App component to use useActivity hook for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useReducer, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Form } from './components/Form';
 import { Header } from './components/Header';
 import ActivityList from './components/ActivityList';
-import { activityReducer, initialSate } from './reducers/activityReducer';
 import { CalorieTracker } from './components/CalorieTracker';
+import { useActivity } from './hooks/useActivity';
 
 function App() {
-  const [state, dispatch] = useReducer(activityReducer, initialSate);
-
+  const { state, dispatch } = useActivity();
+  
   // Load from localStorage on app initialization
   useEffect(() => {
     localStorage.setItem('activities', JSON.stringify(state.activities));
